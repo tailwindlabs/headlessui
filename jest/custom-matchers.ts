b2a33a2d7a431e73e9a81fe31f0345b6e880a115
Robin Malfait <malfait.robin@gmail.com>
2020-09-23 09:42:38 +0200

    add @testing-library/jest-dom for custom matchers
    This way we can use expect(element).toHaveAttribute(key, value?)

--- jest/custom-matchers.ts
+++ jest/custom-matchers.ts
@@ -1,31 +1,31 @@
+import '@testing-library/jest-dom/extend-expect'
+
 // Assuming requestAnimationFrame is roughly 60 frames per second
 const frame = 1000 / 60
+const amountOfFrames = 2
 
 const formatter = new Intl.NumberFormat('en')
 
 expect.extend({
   toBeWithinRenderFrame(actual, expected) {
-    const min = expected - frame
-    const max = expected + frame
+    const min = expected - frame * amountOfFrames
+    const max = expected + frame * amountOfFrames
 
     const pass = actual >= min && actual <= max
 
-    if (pass) {
-      return {
-        message: () =>
-          `expected ${actual} not to be within range of a frame ${formatter.format(
-            min
-          )} - ${formatter.format(max)}`,
-        pass: true,
-      }
-    } else {
-      return {
-        message: () =>
-          `expected ${actual} to be within range of a frame ${formatter.format(
-            min
-          )} - ${formatter.format(max)}`,
-        pass: false,
-      }
+    return {
+      message: pass
+        ? () => {
+            return `expected ${actual} not to be within range of a frame ${formatter.format(
+              min
+            )} - ${formatter.format(max)}`
+          }
+        : () => {
+            return `expected ${actual} not to be within range of a frame ${formatter.format(
+              min
+            )} - ${formatter.format(max)}`
+          },
+      pass,
     }
   },
 })
