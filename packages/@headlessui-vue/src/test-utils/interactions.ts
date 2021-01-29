1d98d7d1c136ab14837a60619ac3c456297e28e2
Robin Malfait <malfait.robin@gmail.com>
2021-01-29 01:18:18 +0100

    temporary fix selector because of JSDOM bug

--- packages/@headlessui-vue/src/test-utils/interactions.ts
+++ packages/@headlessui-vue/src/test-utils/interactions.ts
@@ -307,7 +307,14 @@ let focusableSelector = [
   'select:not([disabled])',
   'textarea:not([disabled])',
 ]
-  .map(selector => `${selector}:not([tabindex='-1'])`)
+  .map(
+    process.env.NODE_ENV === 'test'
+      ? // TODO: Remove this once JSDOM fixes the issue where an element that is
+        // "hidden" can be the document.activeElement, because this is not possible
+        // in real browsers.
+        selector => `${selector}:not([tabindex='-1']):not([style*='display: none'])`
+      : selector => `${selector}:not([tabindex='-1'])`
+  )
   .join(',')
 
 function getFocusableElements(container = document.body) {
