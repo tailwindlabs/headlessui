7b9827f7ba350b4252678e889ee3229a884feed1
Robin Malfait <malfait.robin@gmail.com>
2021-01-29 01:20:10 +0100

    expose FocusTrap

--- packages/@headlessui-react/src/index.ts
+++ packages/@headlessui-react/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/disclosure/disclosure'
+export * from './components/focus-trap/focus-trap'
 export * from './components/listbox/listbox'
 export * from './components/menu/menu'
 export * from './components/switch/switch'
