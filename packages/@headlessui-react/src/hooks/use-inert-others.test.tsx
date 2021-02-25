87b510365452ca41187ac569ffc63e50163d28d3
Robin Malfait <malfait.robin@gmail.com>
2021-02-25 19:10:48 +0100

    ensure useInertOthers works in multiple places
    Previously each hook call would take care of the whole tree. However
    when multiple calls to this hook are happening we need to make sure that
    you are not removing the aria-hidden when another hook is still used.
    
    This will fix that by keeping track of a list of "interactable" items,
    and updating the parents (root of the body) accordingly.

---
+++ packages/@headlessui-react/src/hooks/use-inert-others.test.tsx
@@ -0,0 +1,295 @@
+import React, { useRef, useState } from 'react'
+import { render } from '@testing-library/react'
+import { useInertOthers } from './use-inert-others'
+import { getByText } from '../test-utils/accessibility-assertions'
+import { click } from '../test-utils/interactions'
+
+beforeEach(() => {
+  jest.restoreAllMocks()
+  jest.spyOn(global.console, 'error').mockImplementation(jest.fn())
+})
+
+it('should be possible to inert other elements', async () => {
+  function Example() {
+    let ref = useRef(null)
+    let [enabled, setEnabled] = useState(true)
+    useInertOthers(ref, enabled)
+
+    return (
+      <div ref={ref} id="main">
+        <button onClick={() => setEnabled(v => !v)}>toggle</button>
+      </div>
+    )
+  }
+
+  function Before() {
+    return <div>before</div>
+  }
+
+  function After() {
+    return <div>after</div>
+  }
+
+  render(
+    <>
+      <Before />
+      <Example />
+      <After />
+    </>,
+    { container: document.body }
+  )
+
+  // Verify the others are hidden
+  expect(document.getElementById('main')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Restore
+  await click(getByText('toggle'))
+
+  // Verify we are un-hidden
+  expect(document.getElementById('main')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+})
+
+it('should restore inert elements, when all useInertOthers calls are disabled', async () => {
+  function Example({ toggle, id }: { toggle: string; id: string }) {
+    let ref = useRef(null)
+    let [enabled, setEnabled] = useState(false)
+    useInertOthers(ref, enabled)
+
+    return (
+      <div ref={ref} id={id}>
+        <button onClick={() => setEnabled(v => !v)}>{toggle}</button>
+      </div>
+    )
+  }
+
+  function Before() {
+    return <div>before</div>
+  }
+
+  function After() {
+    return <div>after</div>
+  }
+
+  render(
+    <>
+      <Before />
+      <Example id="main1" toggle="toggle 1" />
+      <Example id="main2" toggle="toggle 2" />
+      <After />
+    </>,
+    { container: document.body }
+  )
+
+  // Verify nothing is hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+
+  // Enable inert on others (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Enable inert on others (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove first level of inert (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove second level of inert (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are not hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+})
+
+it('should restore inert elements, when all useInertOthers calls are disabled (including parents)', async () => {
+  function Example({ toggle, id }: { toggle: string; id: string }) {
+    let ref = useRef(null)
+    let [enabled, setEnabled] = useState(false)
+    useInertOthers(ref, enabled)
+
+    return (
+      <div id={`parent-${id}`}>
+        <div ref={ref} id={id}>
+          <button onClick={() => setEnabled(v => !v)}>{toggle}</button>
+        </div>
+      </div>
+    )
+  }
+
+  function Before() {
+    return <div>before</div>
+  }
+
+  function After() {
+    return <div>after</div>
+  }
+
+  render(
+    <>
+      <Before />
+      <Example id="main1" toggle="toggle 1" />
+      <Example id="main2" toggle="toggle 2" />
+      <After />
+    </>,
+    { container: document.body }
+  )
+
+  // Verify nothing is hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+
+  // Enable inert on others (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden', 'true')
+  expect(document.getElementById('parent-main2')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Enable inert on others (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove first level of inert (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main1')).toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove second level of inert (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are not hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent-main2')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+})
+
+it('should handle inert others correctly when 2 useInertOthers are used in a shared parent', async () => {
+  function Example({ toggle, id }: { toggle: string; id: string }) {
+    let ref = useRef(null)
+    let [enabled, setEnabled] = useState(false)
+    useInertOthers(ref, enabled)
+
+    return (
+      <div ref={ref} id={id}>
+        <button onClick={() => setEnabled(v => !v)}>{toggle}</button>
+      </div>
+    )
+  }
+
+  function Before() {
+    return <div>before</div>
+  }
+
+  function After() {
+    return <div>after</div>
+  }
+
+  render(
+    <>
+      <Before />
+      <div id="parent">
+        <Example id="main1" toggle="toggle 1" />
+        <Example id="main2" toggle="toggle 2" />
+      </div>
+      <After />
+    </>,
+    { container: document.body }
+  )
+
+  // Verify nothing is hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+
+  // Enable inert on others (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden', 'true')
+  expect(document.getElementById('parent')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Enable inert on others (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove first level of inert (via toggle 1)
+  await click(getByText('toggle 1'))
+
+  // Verify the others are hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).toHaveAttribute('aria-hidden', 'true')
+  expect(getByText('after')).toHaveAttribute('aria-hidden', 'true')
+
+  // Remove second level of inert (via toggle 2)
+  await click(getByText('toggle 2'))
+
+  // Verify the others are not hidden
+  expect(document.getElementById('main1')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('main2')).not.toHaveAttribute('aria-hidden')
+  expect(document.getElementById('parent')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('before')).not.toHaveAttribute('aria-hidden')
+  expect(getByText('after')).not.toHaveAttribute('aria-hidden')
+})
--- packages/@headlessui-react/src/hooks/use-inert-others.ts
+++ packages/@headlessui-react/src/hooks/use-inert-others.ts
@@ -1,14 +1,23 @@
 import { MutableRefObject } from 'react'
 import { useIsoMorphicEffect } from './use-iso-morphic-effect'
 
-function* getAllSiblings(element: HTMLElement) {
-  if (!element.parentElement) return
-  let node = element.parentElement.firstChild
+let interactables = new Set<HTMLElement>()
+let originals = new Map<HTMLElement, { 'aria-hidden': string | null; inert: boolean }>()
 
-  while (node) {
-    if (node !== element && node instanceof HTMLElement) yield node
-    node = node.nextSibling
-  }
+function inert(element: HTMLElement) {
+  element.setAttribute('aria-hidden', 'true')
+  // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
+  element.inert = true
+}
+
+function restore(element: HTMLElement) {
+  let original = originals.get(element)
+  if (!original) return
+
+  if (original['aria-hidden'] === null) element.removeAttribute('aria-hidden')
+  else element.setAttribute('aria-hidden', original['aria-hidden'])
+  // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
+  element.inert = original.inert
 }
 
 export function useInertOthers<TElement extends HTMLElement>(
@@ -20,47 +29,77 @@ export function useInertOthers<TElement extends HTMLElement>(
     if (!container.current) return
 
     let element = container.current
-    let elements = new Map<HTMLElement, { 'aria-hidden': string | null; inert: boolean }>()
 
-    // Collect my direct siblings
-    for (let sibling of getAllSiblings(element)) {
-      elements.set(sibling, {
-        'aria-hidden': sibling.getAttribute('aria-hidden'),
-        // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
-        inert: sibling.inert,
-      })
+    // Mark myself as an interactable element
+    interactables.add(element)
+
+    // Restore elements that now contain an interactable child
+    for (let original of originals.keys()) {
+      if (original.contains(element)) {
+        restore(original)
+        originals.delete(original)
+      }
     }
 
     // Collect direct children of the body
-    document.querySelectorAll('body > *').forEach(directChild => {
-      if (directChild === element) return // Skip myself
-      if (!(directChild instanceof HTMLElement)) return // Skip non-HTMLElements
-      if (directChild.contains(element)) return // Skip my parent
+    document.querySelectorAll('body > *').forEach(child => {
+      if (!(child instanceof HTMLElement)) return // Skip non-HTMLElements
 
-      elements.set(directChild, {
-        'aria-hidden': directChild.getAttribute('aria-hidden'),
-        // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
-        inert: directChild.inert,
-      })
-    })
-
-    // MUTATE ALL THE ELEMENTS
-    for (let element of elements.keys()) {
-      element.setAttribute('aria-hidden', 'true')
-      // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
-      element.inert = true
-    }
-
-    return () => {
-      for (let [element, bag] of elements.entries()) {
-        if (element === null) continue
-        else if (bag['aria-hidden'] === null) element.removeAttribute('aria-hidden')
-        else element.setAttribute('aria-hidden', bag['aria-hidden'])
-        // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
-        element.inert = bag.inert
+      // Skip the interactables, and the parents of the interactables
+      for (let interactable of interactables) {
+        if (child.contains(interactable)) return
       }
 
-      elements.clear()
+      // Keep track of the elements
+      if (interactables.size === 1) {
+        originals.set(child, {
+          'aria-hidden': child.getAttribute('aria-hidden'),
+          // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
+          inert: child.inert,
+        })
+
+        // Mutate the element
+        inert(child)
+      }
+    })
+
+    return () => {
+      // Inert is disabled on the current element
+      interactables.delete(element)
+
+      // We still have interactable elements, therefore this one and its parent
+      // will become inert as well.
+      if (interactables.size > 0) {
+        // Collect direct children of the body
+        document.querySelectorAll('body > *').forEach(child => {
+          if (!(child instanceof HTMLElement)) return // Skip non-HTMLElements
+
+          // Skip already inert parents
+          if (originals.has(child)) return
+
+          // Skip the interactables, and the parents of the interactables
+          for (let interactable of interactables) {
+            if (child.contains(interactable)) return
+          }
+
+          originals.set(child, {
+            'aria-hidden': child.getAttribute('aria-hidden'),
+            // @ts-expect-error `inert` does not exist on HTMLElement (yet!)
+            inert: child.inert,
+          })
+
+          // Mutate the element
+          inert(child)
+        })
+      } else {
+        for (let element of originals.keys()) {
+          // Restore
+          restore(element)
+
+          // Cleanup
+          originals.delete(element)
+        }
+      }
     }
   }, [enabled])
 }
