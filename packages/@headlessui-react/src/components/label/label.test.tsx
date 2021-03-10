ffc68b17c21602e0d5128a2c043065726ae4414c
Robin Malfait <malfait.robin@gmail.com>
2021-03-10 11:36:35 +0100

    add internal Label component

---
+++ packages/@headlessui-react/src/components/label/label.test.tsx
@@ -0,0 +1,108 @@
+import { render } from '@testing-library/react'
+import { Label, useLabels } from './label'
+import React, { ReactNode } from 'react'
+
+jest.mock('../../hooks/use-id')
+
+it('should be possible to use a LabelProvider without using a Label', async () => {
+  function Component(props: { children: ReactNode }) {
+    let [labelledby, LabelProvider] = useLabels()
+
+    return (
+      <LabelProvider>
+        <div aria-labelledby={labelledby}>{props.children}</div>
+      </LabelProvider>
+    )
+  }
+
+  function Example() {
+    return <Component>No label</Component>
+  }
+
+  let { container } = render(<Example />)
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div>
+      No label
+    </div>
+  `)
+})
+
+it('should be possible to use a LabelProvider and a single Label, and have them linked', async () => {
+  function Component(props: { children: ReactNode }) {
+    let [labelledby, LabelProvider] = useLabels()
+
+    return (
+      <LabelProvider>
+        <div aria-labelledby={labelledby}>{props.children}</div>
+      </LabelProvider>
+    )
+  }
+
+  function Example() {
+    return (
+      <Component>
+        <Label>I am a label</Label>
+        <span>Contents</span>
+      </Component>
+    )
+  }
+
+  let { container } = render(<Example />)
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div
+      aria-labelledby="headlessui-label-1"
+    >
+      <label
+        id="headlessui-label-1"
+      >
+        I am a label
+      </label>
+      <span>
+        Contents
+      </span>
+    </div>
+  `)
+})
+
+it('should be possible to use a LabelProvider and multiple Label ocmponents, and have them linked', async () => {
+  function Component(props: { children: ReactNode }) {
+    let [labelledby, LabelProvider] = useLabels()
+
+    return (
+      <LabelProvider>
+        <div aria-labelledby={labelledby}>{props.children}</div>
+      </LabelProvider>
+    )
+  }
+
+  function Example() {
+    return (
+      <Component>
+        <Label>I am a label</Label>
+        <span>Contents</span>
+        <Label>I am also a label</Label>
+      </Component>
+    )
+  }
+
+  let { container } = render(<Example />)
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div
+      aria-labelledby="headlessui-label-1 headlessui-label-2"
+    >
+      <label
+        id="headlessui-label-1"
+      >
+        I am a label
+      </label>
+      <span>
+        Contents
+      </span>
+      <label
+        id="headlessui-label-2"
+      >
+        I am also a label
+      </label>
+    </div>
+  `)
+})
---
+++ packages/@headlessui-react/src/components/label/label.tsx
@@ -0,0 +1,79 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+
+  // Types
+  ElementType,
+  ReactNode,
+} from 'react'
+
+import { Props } from '../../types'
+import { useId } from '../../hooks/use-id'
+import { render } from '../../utils/render'
+import { useIsoMorphicEffect } from '../../hooks/use-iso-morphic-effect'
+
+// ---
+
+let LabelContext = createContext<{ register(value: string): () => void }>({
+  register() {
+    return () => {}
+  },
+})
+
+function useLabelContext() {
+  return useContext(LabelContext)
+}
+
+export function useLabels(): [string | undefined, (props: { children: ReactNode }) => JSX.Element] {
+  let [labelIds, setLabelIds] = useState<string[]>([])
+
+  return [
+    // The actual id's as string or undefined
+    labelIds.length > 0 ? labelIds.join(' ') : undefined,
+
+    // The provider component
+    useMemo(() => {
+      return function LabelProvider(props: { children: ReactNode }) {
+        let register = useCallback((value: string) => {
+          setLabelIds(existing => [...existing, value])
+
+          return () =>
+            setLabelIds(existing => {
+              let clone = existing.slice()
+              let idx = clone.indexOf(value)
+              if (idx !== -1) clone.splice(idx, 1)
+              return clone
+            })
+        }, [])
+
+        let contextBag = useMemo(() => ({ register }), [register])
+
+        return <LabelContext.Provider value={contextBag}>{props.children}</LabelContext.Provider>
+      }
+    }, [setLabelIds]),
+  ]
+}
+
+// ---
+
+let DEFAULT_LABEL_TAG = 'label' as const
+interface LabelRenderPropArg {}
+type LabelPropsWeControl = 'id'
+
+export function Label<TTag extends ElementType = typeof DEFAULT_LABEL_TAG>(
+  props: Props<TTag, LabelRenderPropArg, LabelPropsWeControl>
+) {
+  let { register } = useLabelContext()
+  let id = `headlessui-label-${useId()}`
+
+  useIsoMorphicEffect(() => register(id), [id, register])
+
+  let passThroughProps = props
+  let propsWeControl = { id }
+  let bag = useMemo<LabelRenderPropArg>(() => ({}), [])
+
+  return render({ ...passThroughProps, ...propsWeControl }, bag, DEFAULT_LABEL_TAG)
+}
