3050dff47580f3a95e960f763cd69232a28aa9fb
Robin Malfait <malfait.robin@gmail.com>
2021-03-22 16:49:06 +0100

    add FocusTrap example

---
+++ packages/@headlessui-vue/examples/src/components/focus-trap/focus-trap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-center w-screen h-full p-12 bg-gray-50">
+    <div class="w-full max-w-xs mx-auto">
+      <button>Previous</button>
+      <FocusTrap>
+        <button>Trigger</button>
+      </FocusTrap>
+      <button>After</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, h, ref, onMounted, watchEffect, watch } from 'vue'
+import { FocusTrap } from '@headlessui/vue'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default {
+  components: { FocusTrap },
+  setup(props, context) {
+    return {}
+  },
+}
+</script>
