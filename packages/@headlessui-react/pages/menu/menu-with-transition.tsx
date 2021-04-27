5176e07fbdfac525f1c9546ed6dc1eab5149d53c
Robin Malfait <malfait.robin@gmail.com>
2021-04-27 14:52:36 +0200

    simplify examples by using the implicit open/closed state

--- packages/@headlessui-react/pages/menu/menu-with-transition.tsx
+++ packages/@headlessui-react/pages/menu/menu-with-transition.tsx
@@ -18,65 +18,57 @@ export default function Home() {
     <div className="flex justify-center w-screen h-full p-12 bg-gray-50">
       <div className="relative inline-block text-left">
         <Menu>
-          {({ open }) => (
-            <>
-              <span className="rounded-md shadow-sm">
-                <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
-                  <span>Options</span>
-                  <svg className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </Menu.Button>
-              </span>
+          <span className="rounded-md shadow-sm">
+            <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800">
+              <span>Options</span>
+              <svg className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Menu.Button>
+          </span>
 
-              <Transition
-                show={open}
-                enter="transition duration-100 ease-out"
-                enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform scale-100 opacity-100"
-                leaveTo="transform scale-95 opacity-0"
-              >
-                <Menu.Items
-                  static
-                  className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-                >
-                  <div className="px-4 py-3">
-                    <p className="text-sm leading-5">Signed in as</p>
-                    <p className="text-sm font-medium leading-5 text-gray-900 truncate">
-                      tom@example.com
-                    </p>
-                  </div>
+          <Transition
+            enter="transition duration-1000 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-1000 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
+              <div className="px-4 py-3">
+                <p className="text-sm leading-5">Signed in as</p>
+                <p className="text-sm font-medium leading-5 text-gray-900 truncate">
+                  tom@example.com
+                </p>
+              </div>
 
-                  <div className="py-1">
-                    <Menu.Item as="a" href="#account-settings" className={resolveClass}>
-                      Account settings
-                    </Menu.Item>
-                    <Menu.Item as="a" href="#support" className={resolveClass}>
-                      Support
-                    </Menu.Item>
-                    <Menu.Item as="a" disabled href="#new-feature" className={resolveClass}>
-                      New feature (soon)
-                    </Menu.Item>
-                    <Menu.Item as="a" href="#license" className={resolveClass}>
-                      License
-                    </Menu.Item>
-                  </div>
+              <div className="py-1">
+                <Menu.Item as="a" href="#account-settings" className={resolveClass}>
+                  Account settings
+                </Menu.Item>
+                <Menu.Item as="a" href="#support" className={resolveClass}>
+                  Support
+                </Menu.Item>
+                <Menu.Item as="a" disabled href="#new-feature" className={resolveClass}>
+                  New feature (soon)
+                </Menu.Item>
+                <Menu.Item as="a" href="#license" className={resolveClass}>
+                  License
+                </Menu.Item>
+              </div>
 
-                  <div className="py-1">
-                    <Menu.Item as="a" href="#sign-out" className={resolveClass}>
-                      Sign out
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </>
-          )}
+              <div className="py-1">
+                <Menu.Item as="a" href="#sign-out" className={resolveClass}>
+                  Sign out
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
         </Menu>
       </div>
     </div>
