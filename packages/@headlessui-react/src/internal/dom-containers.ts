e344f20c65942d45ac1610fdfbcdfacbcdb3f22e
Robin Malfait <malfait.robin@gmail.com>
2021-05-07 12:19:51 +0200

    drop dom-containers
    Currently it is only used in a single spot, so I inlined it into that
    file.

--- packages/@headlessui-react/src/internal/dom-containers.ts
+++
@@ -1,7 +0,0 @@
-export function contains(containers: Set<HTMLElement>, element: HTMLElement) {
-  for (let container of containers) {
-    if (container.contains(element)) return true
-  }
-
-  return false
-}
