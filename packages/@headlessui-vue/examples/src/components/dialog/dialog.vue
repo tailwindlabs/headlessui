aab2da646a454d0c93846e176b53521b308383f0
Sandeep Ramgolam <sandeep@ramgolam.com>
2021-05-12 06:11:36 +0000

    Add missing :open prop to Dialog example
    It will complain if the prop is not present
    
    > Uncaught (in promise) Error: You forgot to provide an `open` prop to the `Dialog`.

--- packages/@headlessui-vue/examples/src/components/dialog/dialog.vue
+++ packages/@headlessui-vue/examples/src/components/dialog/dialog.vue
@@ -8,7 +8,7 @@
   </button>
 
   <TransitionRoot :show="isOpen" as="template">
-    <Dialog @close="setIsOpen">
+    <Dialog :open="isOpen" @close="setIsOpen">
       <div class="fixed z-10 inset-0 overflow-y-auto">
         <div
           class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
