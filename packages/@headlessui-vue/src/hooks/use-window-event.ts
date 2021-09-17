aa1a48cbfe518804a05418bf018b6808c494616b
Robin Malfait <malfait.robin@gmail.com>
2021-09-17 13:52:18 +0200

    Use `watchEffect` instead of immediately using an event listener on `window` (#817)
    * use `watchEffect` instead of immediately using an event listener on `window`
    
    * update changelog

--- packages/@headlessui-vue/src/hooks/use-window-event.ts
+++ packages/@headlessui-vue/src/hooks/use-window-event.ts
@@ -1,10 +1,15 @@
-import { onUnmounted } from 'vue'
+import { watchEffect } from 'vue'
 
 export function useWindowEvent<TType extends keyof WindowEventMap>(
   type: TType,
   listener: (this: Window, ev: WindowEventMap[TType]) => any,
   options?: boolean | AddEventListenerOptions
 ) {
-  window.addEventListener(type, listener, options)
-  onUnmounted(() => window.removeEventListener(type, listener, options))
+  watchEffect(onInvalidate => {
+    window.addEventListener(type, listener, options)
+
+    onInvalidate(() => {
+      window.removeEventListener(type, listener, options)
+    })
+  })
 }
