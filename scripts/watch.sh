dc6a9e4a9aea9b90c248f09e8906f61f267069dc
Robin Malfait <malfait.robin@gmail.com>
2022-01-21 01:51:30 +0100

    add dedicated watch script

---
+++ scripts/watch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Known variables
+outdir="./dist"
+name="headlessui"
+input="./src/index.ts"
+
+# Find executables
+esbuild=$(yarn bin esbuild)
+tsc=$(yarn bin tsc)
+
+# Setup shared options for esbuild
+sharedOptions=()
+sharedOptions+=("--bundle")
+sharedOptions+=("--platform=browser")
+sharedOptions+=("--target=es2020")
+
+# Generate actual builds
+$esbuild $input --format=esm  --outfile=$outdir/$name.esm.js --sourcemap ${sharedOptions[@]} $@ --watch
+
