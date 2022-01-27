947e03e9af069bfcbd5b16a3d06b392b1308bd7c
Robin Malfait <malfait.robin@gmail.com>
2022-01-27 11:46:26 +0100

    use `useLatestValue` hook

--- packages/@headlessui-react/src/hooks/use-computed.ts
+++ packages/@headlessui-react/src/hooks/use-computed.ts
@@ -1,12 +1,10 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { useIsoMorphicEffect } from './use-iso-morphic-effect'
+import { useLatestValue } from './use-latest-value'
 
 export function useComputed<T>(cb: () => T, dependencies: React.DependencyList) {
   let [value, setValue] = useState(cb)
-  let cbRef = useRef(cb)
-  useIsoMorphicEffect(() => {
-    cbRef.current = cb
-  }, [cb])
+  let cbRef = useLatestValue(cb)
   useIsoMorphicEffect(() => setValue(cbRef.current), [cbRef, setValue, ...dependencies])
   return value
 }
