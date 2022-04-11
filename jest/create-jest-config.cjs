11e1fe5d94037ba9aa0c47eb04ecb5fb4c4a2cd9
Robin Malfait <malfait.robin@gmail.com>
2022-04-11 23:30:08 +0200

    setup Jest to include `IS_REACT_ACT_ENVIRONMENT`

--- jest/create-jest-config.cjs
+++ jest/create-jest-config.cjs
@@ -1,12 +1,14 @@
 module.exports = function createJestConfig(root, options) {
+  let { setupFilesAfterEnv = [], transform = {}, ...rest } = options
   return Object.assign(
     {
       rootDir: root,
-      setupFilesAfterEnv: ['<rootDir>../../jest/custom-matchers.ts'],
+      setupFilesAfterEnv: ['<rootDir>../../jest/custom-matchers.ts', ...setupFilesAfterEnv],
       transform: {
         '^.+\\.(t|j)sx?$': '@swc/jest',
+        ...transform,
       },
     },
-    options
+    rest
   )
 }
