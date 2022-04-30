f7bdc7743149b73ad9a2d873b04910378c322562
Robin Malfait <malfait.robin@gmail.com>
2022-04-30 18:36:16 +0200

    WIP

--- packages/browser-tests/playwright.config.ts
+++ packages/browser-tests/playwright.config.ts
@@ -11,14 +11,14 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 }
 
