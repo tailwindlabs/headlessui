b8c08d5cbbb0f740b1a9b505d9c1db7db277deff
Валерий Иванов <va.ivanov@cian.ru>
2022-05-15 21:22:19 +0300

    Next playground <Head> fix

---
+++ packages/playground-react/pages/_document.tsx
@@ -0,0 +1,32 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography"></script>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://headlessui.dev/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://headlessui.dev/favicon-16x16.png"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
