6fd5f58c2c8e2092ef30be07a6f200574f30b46e
Robin Malfait <malfait.robin@gmail.com>
2022-05-16 15:49:50 +0200

    ensure standalone imports and exports are rewritten as well (#1458)

--- scripts/rewrite-imports.js
+++ scripts/rewrite-imports.js
@@ -8,7 +8,7 @@ console.time('Rewrote imports in')
 fastGlob.sync([process.argv.slice(2).join('')]).forEach((file) => {
   file = path.resolve(process.cwd(), file)
   let content = fs.readFileSync(file, 'utf8')
-  let result = content.replace(/from([^"']*?)(["'])\.(.*?)\2/g, 'from$1".$3.js"')
+  let result = content.replace(/(import|export)([^"']*?)(["'])\.(.*?)\3;/g, '$1$2".$4.js";')
   if (result !== content) {
     fs.writeFileSync(file, result, 'utf8')
   }
