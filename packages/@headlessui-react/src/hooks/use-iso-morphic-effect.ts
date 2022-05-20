5a3431d835e419c0f313663019491dc8ab75ca6c
Robin Malfait <malfait.robin@gmail.com>
2022-05-21 01:41:01 +0200

    convert `const` to `let`
    Just for consistency..

--- packages/@headlessui-react/src/hooks/use-iso-morphic-effect.ts
+++ packages/@headlessui-react/src/hooks/use-iso-morphic-effect.ts
@@ -1,3 +1,3 @@
 import { useLayoutEffect, useEffect } from 'react'
 
-export const useIsoMorphicEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
+export let useIsoMorphicEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
