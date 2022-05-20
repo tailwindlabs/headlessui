e9c0780415e1f96008c1f9716bbc43a877eb3e3d
Robin Malfait <malfait.robin@gmail.com>
2022-05-21 01:22:07 +0200

    improve type signature of the `useEvent` hook

--- packages/@headlessui-react/src/hooks/use-event.ts
+++ packages/@headlessui-react/src/hooks/use-event.ts
@@ -2,8 +2,12 @@ import React from 'react'
 
 export let useEvent =
   // TODO: Add React.useEvent ?? once the useEvent hook is available
-  function useEvent<T, R>(cb: (...args: T[]) => R) {
+  function useEvent<
+    F extends (...args: any[]) => any,
+    P extends any[] = Parameters<F>,
+    R = ReturnType<F>
+  >(cb: (...args: P) => R) {
     let cache = React.useRef(cb)
     cache.current = cb
-    return React.useCallback((...args: T[]) => cache.current(...args), [cache])
+    return React.useCallback((...args: P) => cache.current(...args), [cache])
   }
