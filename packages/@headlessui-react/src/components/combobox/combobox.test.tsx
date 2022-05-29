1d53ac312f9f4423b8539253c7d67cd93a08fdc0
Robin Malfait <malfait.robin@gmail.com>
2022-05-29 14:57:35 +0200

    Revert "prepare 1.6.4"
    This reverts commit 842d07146ed4b6428ed7db090d10c481aa8670da.

--- packages/@headlessui-react/src/components/combobox/combobox.test.tsx
+++ packages/@headlessui-react/src/components/combobox/combobox.test.tsx
@@ -171,7 +171,7 @@ describe('Rendering', () => {
       })
     )
 
-    describe.skip('Equality', () => {
+    describe('Equality', () => {
       let options = [
         { id: 1, name: 'Alice' },
         { id: 2, name: 'Bob' },
