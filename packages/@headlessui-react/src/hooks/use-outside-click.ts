979de55efe0eac3873ae8ce91195b5430ac8d963
Jordan Pittman <jordan@cryptica.me>
2022-06-03 17:09:50 -0400

    Refactor

--- packages/@headlessui-react/src/hooks/use-outside-click.ts
+++ packages/@headlessui-react/src/hooks/use-outside-click.ts
@@ -26,64 +26,70 @@ export function useOutsideClick(
     [enabled]
   )
 
+  function handleOutsideClick<E extends MouseEvent | PointerEvent>(
+    event: E,
+    resolveTarget: (event: E) => HTMLElement
+  ) {
+    if (!enabledRef.current) return
+
+    // Check whether the event got prevented already. This can happen if you use the
+    // useOutsideClick hook in both a Dialog and a Menu and the inner Menu "cancels" the default
+    // behaviour so that only the Menu closes and not the Dialog (yet)
+    if (event.defaultPrevented) return
+
+    let _containers = (function resolve(containers): ContainerCollection {
+      if (typeof containers === 'function') {
+        return resolve(containers())
+      }
+
+      if (Array.isArray(containers)) {
+        return containers
+      }
+
+      if (containers instanceof Set) {
+        return containers
+      }
+
+      return [containers]
+    })(containers)
+
+    let target = resolveTarget(event)
+
+    // Ignore if the target doesn't exist in the DOM anymore
+    if (!target.ownerDocument.documentElement.contains(target)) return
+
+    // Ignore if the target exists in one of the containers
+    for (let container of _containers) {
+      if (container === null) continue
+      let domNode = container instanceof HTMLElement ? container : container.current
+      if (domNode?.contains(target)) {
+        return
+      }
+    }
+
+    // This allows us to check whether the event was defaultPrevented when you are nesting this
+    // inside a `<Dialog />` for example.
+    if (
+      // This check alllows us to know whether or not we clicked on a "focusable" element like a
+      // button or an input. This is a backwards compatibility check so that you can open a <Menu
+      // /> and click on another <Menu /> which should close Menu A and open Menu B. We might
+      // revisit that so that you will require 2 clicks instead.
+      !isFocusableElement(target, FocusableMode.Loose) &&
+      // This could be improved, but the `Combobox.Button` adds tabIndex={-1} to make it
+      // unfocusable via the keyboard so that tabbing to the next item from the input doesn't
+      // first go to the button.
+      target.tabIndex !== -1
+    ) {
+      event.preventDefault()
+    }
+
+    return cb(event, target)
+  }
+
   useWindowEvent(
     'click',
-    (event) => {
-      if (!enabledRef.current) return
+    (event) => handleOutsideClick(event, (event) => event.target as HTMLElement),
 
-      // Check whether the event got prevented already. This can happen if you use the
-      // useOutsideClick hook in both a Dialog and a Menu and the inner Menu "cancels" the default
-      // behaviour so that only the Menu closes and not the Dialog (yet)
-      if (event.defaultPrevented) return
-
-      let _containers = (function resolve(containers): ContainerCollection {
-        if (typeof containers === 'function') {
-          return resolve(containers())
-        }
-
-        if (Array.isArray(containers)) {
-          return containers
-        }
-
-        if (containers instanceof Set) {
-          return containers
-        }
-
-        return [containers]
-      })(containers)
-
-      let target = event.target as HTMLElement
-
-      // Ignore if the target doesn't exist in the DOM anymore
-      if (!target.ownerDocument.documentElement.contains(target)) return
-
-      // Ignore if the target exists in one of the containers
-      for (let container of _containers) {
-        if (container === null) continue
-        let domNode = container instanceof HTMLElement ? container : container.current
-        if (domNode?.contains(target)) {
-          return
-        }
-      }
-
-      // This allows us to check whether the event was defaultPrevented when you are nesting this
-      // inside a `<Dialog />` for example.
-      if (
-        // This check alllows us to know whether or not we clicked on a "focusable" element like a
-        // button or an input. This is a backwards compatibility check so that you can open a <Menu
-        // /> and click on another <Menu /> which should close Menu A and open Menu B. We might
-        // revisit that so that you will require 2 clicks instead.
-        !isFocusableElement(target, FocusableMode.Loose) &&
-        // This could be improved, but the `Combobox.Button` adds tabIndex={-1} to make it
-        // unfocusable via the keyboard so that tabbing to the next item from the input doesn't
-        // first go to the button.
-        target.tabIndex !== -1
-      ) {
-        event.preventDefault()
-      }
-
-      return cb(event, target)
-    },
     // We will use the `capture` phase so that layers in between with `event.stopPropagation()`
     // don't "cancel" this outside click check. E.g.: A `Menu` inside a `DialogPanel` if the `Menu`
     // is open, and you click outside of it in the `DialogPanel` the `Menu` should close. However,
