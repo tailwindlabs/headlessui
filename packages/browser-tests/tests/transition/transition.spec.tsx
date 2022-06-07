4979f9ca32291e6c10d4b59a175217e3221bc7cf
Jordan Pittman <jordan@cryptica.me>
2022-06-07 17:07:13 -0400

    WIP

--- packages/browser-tests/tests/transition/transition.spec.tsx
+++ packages/browser-tests/tests/transition/transition.spec.tsx
@@ -60,10 +60,13 @@ test('root: should cancel transitions', async ({ render, page, animations }) => 
   await animations.wait()
 
   expect(animations.length).toEqual(2)
+
+  expect(animations[0].target).toEqual('root')
   expect(animations[0].state).toEqual('cancelled')
   expect(animations[0].elapsedTime).toBeLessThan(50)
   expect(animations[0].properties).toEqual(['opacity'])
 
+  expect(animations[1].target).toEqual('root')
   expect(animations[1].state).toEqual('ended')
   expect(animations[1].elapsedTime).toBeLessThan(50)
   expect(animations[1].properties).toEqual(['opacity'])
@@ -89,26 +92,32 @@ test('children: should transition in and out completely', async ({ render, page,
 
   expect(animations.length).toEqual(6)
 
+  expect(animations[0].target).toEqual('child-1')
   expect(animations[0].state).toEqual('ended')
   expect(animations[0].elapsedTime).toEqual(50)
   expect(animations[0].properties).toEqual(['opacity'])
 
+  expect(animations[1].target).toEqual('child-2')
   expect(animations[1].state).toEqual('ended')
   expect(animations[1].elapsedTime).toEqual(50)
   expect(animations[1].properties).toEqual(['opacity'])
 
+  expect(animations[2].target).toEqual('root')
   expect(animations[2].state).toEqual('ended')
   expect(animations[2].elapsedTime).toEqual(50)
   expect(animations[2].properties).toEqual(['opacity'])
 
+  expect(animations[3].target).toEqual('child-1')
   expect(animations[3].state).toEqual('ended')
   expect(animations[3].elapsedTime).toEqual(50)
   expect(animations[3].properties).toEqual(['opacity'])
 
+  expect(animations[4].target).toEqual('child-2')
   expect(animations[4].state).toEqual('ended')
   expect(animations[4].elapsedTime).toEqual(50)
   expect(animations[4].properties).toEqual(['opacity'])
 
+  expect(animations[5].target).toEqual('root')
   expect(animations[5].state).toEqual('ended')
   expect(animations[5].elapsedTime).toEqual(50)
   expect(animations[5].properties).toEqual(['opacity'])
@@ -134,26 +143,32 @@ test('children: should cancel transitions', async ({ render, page, animations })
 
   expect(animations.length).toEqual(6)
 
+  expect(animations[0].target).toEqual('child-1')
   expect(animations[0].state).toEqual('cancelled')
   expect(animations[0].elapsedTime).toBeLessThan(50)
   expect(animations[0].properties).toEqual(['opacity'])
 
+  expect(animations[1].target).toEqual('child-2')
   expect(animations[1].state).toEqual('cancelled')
   expect(animations[1].elapsedTime).toBeLessThan(50)
   expect(animations[1].properties).toEqual(['opacity'])
 
+  expect(animations[2].target).toEqual('root')
   expect(animations[2].state).toEqual('cancelled')
   expect(animations[2].elapsedTime).toBeLessThan(50)
   expect(animations[2].properties).toEqual(['opacity'])
 
+  expect(animations[3].target).toEqual('child-1')
   expect(animations[3].state).toEqual('ended')
   expect(animations[3].elapsedTime).toBeLessThan(50)
   expect(animations[3].properties).toEqual(['opacity'])
 
+  expect(animations[4].target).toEqual('child-2')
   expect(animations[4].state).toEqual('ended')
   expect(animations[4].elapsedTime).toBeLessThan(50)
   expect(animations[4].properties).toEqual(['opacity'])
 
+  expect(animations[5].target).toEqual('root')
   expect(animations[5].state).toEqual('ended')
   expect(animations[5].elapsedTime).toBeLessThan(50)
   expect(animations[5].properties).toEqual(['opacity'])
