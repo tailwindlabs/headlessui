d11c7ac18ea0193c81428e0962e0d0d064eb69a1
Jordan Pittman <jordan@cryptica.me>
2022-06-30 15:18:40 -0400

    WIP

---
+++ packages/browser-tests/playwright.config.ts
@@ -0,0 +1,25 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+}
+
+export default config
