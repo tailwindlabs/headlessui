b6b1414f3714efbc1ea1818c580be4974b086bf6
Robin Malfait <malfait.robin@gmail.com>
2022-07-27 13:15:20 +0200

    add idempotency comment

--- scripts/rewrite-imports.js
+++ scripts/rewrite-imports.js
@@ -9,6 +9,8 @@ fastGlob.sync([process.argv.slice(2).join('')]).forEach((file) => {
   file = path.resolve(process.cwd(), file)
   let content = fs.readFileSync(file, 'utf8')
   let result = content.replace(/(import|export)([^"']*?)(["'])\.(.*?)\3/g, (full, a, b, _, d) => {
+    // For idempotency reasons, if `.js` already exists, then we can skip this. This allows us to
+    // run this script over and over again without adding .js files every time.
     if (d.endsWith('.js')) {
       return full
     }
