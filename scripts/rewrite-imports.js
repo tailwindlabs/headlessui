bd0213ecfb674d2183672f06661b7fa6167566c1
Robin Malfait <malfait.robin@gmail.com>
2022-07-27 12:46:11 +0200

    update script to mass-update import/export statements

--- scripts/rewrite-imports.js
+++ scripts/rewrite-imports.js
@@ -8,7 +8,13 @@ console.time('Rewrote imports in')
 fastGlob.sync([process.argv.slice(2).join('')]).forEach((file) => {
   file = path.resolve(process.cwd(), file)
   let content = fs.readFileSync(file, 'utf8')
-  let result = content.replace(/(import|export)([^"']*?)(["'])\.(.*?)\3;/g, '$1$2".$4.js";')
+  let result = content.replace(/(import|export)([^"']*?)(["'])\.(.*?)\3/g, (full, a, b, _, d) => {
+    if (d.endsWith('.js')) {
+      return full
+    }
+
+    return `${a}${b}'.${d}.js'`
+  })
   if (result !== content) {
     fs.writeFileSync(file, result, 'utf8')
   }
