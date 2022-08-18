22e9e6fc2cd750eb05f615235e787dbeb33f2a62
Jordan Pittman <jordan@cryptica.me>
2022-08-18 12:20:32 -0400

    Simplify

--- packages/playground-vue/src/components/Home.vue
+++ packages/playground-vue/src/components/Home.vue
@@ -1,47 +1,42 @@
+<script setup>
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+let Examples = defineComponent({
+  props: ['routes'],
+  setup:
+    (props, { slots }) =>
+    () =>
+      slots.default({ routes: props.routes, slots }),
+})
+
+let router = useRouter()
+let routes = router
+  .getRoutes()
+  .filter((example) => example.path !== '/')
+  .filter((route) => route.meta.isRoot)
+</script>
+
 <template>
   <div class="container mx-auto my-24">
     <div class="prose">
       <h2>Examples</h2>
-      <Examples :examples="examples">
-        <slot></slot>
+      <Examples :routes="routes" v-slot="{ routes, slots }">
+        <ul>
+          <li v-for="{ children, meta, path } in routes">
+            <template v-if="children.length > 0">
+              <h3 class="text-xl">{{ meta.name }}</h3>
+              <!-- This is a bit cursed but it works -->
+              <component v-for="vnode in slots.default({ routes: children, slots })" :is="vnode" />
+            </template>
+            <template v-else>
+              <router-link :key="path" :to="path">
+                {{ meta.name }}
+              </router-link>
+            </template>
+          </li>
+        </ul>
       </Examples>
     </div>
   </div>
 </template>
-
-<script>
-import { defineComponent, h } from 'vue'
-import { RouterLink, useRouter } from 'vue-router'
-
-let Examples = defineComponent({
-  props: ['examples'],
-  setup: (props) => () =>
-    h(
-      'ul',
-      props.examples.map((example) =>
-        h(
-          'li',
-          { key: example.path },
-          example.children.length > 0
-            ? h('h3', { class: 'text-xl' }, example.meta.name)
-            : [h(RouterLink, { to: example.path }, () => example.meta.name)],
-          example.children.length > 0 && h(Examples, { examples: example.children })
-        )
-      )
-    ),
-})
-
-export default {
-  components: { Examples },
-  setup() {
-    let router = useRouter()
-
-    return {
-      examples: router
-        .getRoutes()
-        .filter((example) => example.path !== '/')
-        .filter((route) => route.meta.isRoot),
-    }
-  },
-}
-</script>
