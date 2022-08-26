f30c7b4acaa9bd7f1cbe8c1067a2ab242fb243d7
Jordan Pittman <jordan@cryptica.me>
2022-08-26 09:14:04 -0400

    fixit

--- packages/@headlessui-vue/src/internal/hidden.ts
+++ packages/@headlessui-vue/src/internal/hidden.ts
@@ -24,11 +24,11 @@ export let Hidden = defineComponent({
       let ourProps = {
         'aria-hidden': (features & Features.Focusable) === Features.Focusable ? true : undefined,
         style: {
-          position: 'absolute',
-          top: 0,
-          left: 0,
+          position: 'fixed',
+          top: 1,
+          left: 1,
           width: 1,
-          height: 1,
+          height: 0,
           padding: 0,
           margin: -1,
           overflow: 'hidden',
