814bfe589d181c8472700839769206373ab6d769
Jordan Pittman <jordan@cryptica.me>
2023-01-26 15:18:14 -0500

    Fix SSR error

--- packages/@headlessui-react/src/hooks/use-store.ts
+++ packages/@headlessui-react/src/hooks/use-store.ts
@@ -2,5 +2,5 @@ import { useSyncExternalStore } from '../use-sync-external-store-shim'
 import { Store } from '../utils/store'
 
 export function useStore<T>(store: Store<T>) {
-  return useSyncExternalStore(store.subscribe, store.getSnapshot)
+  return useSyncExternalStore(store.subscribe, store.getSnapshot, store.getSnapshot)
 }
