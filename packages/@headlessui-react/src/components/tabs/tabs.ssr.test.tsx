b29eac39521dae04f8786af7f9104b08036ba15a
Jordan Pittman <jordan@cryptica.me>
2023-01-30 11:45:43 -0500

    Work on SSR tests for react

--- packages/@headlessui-react/src/components/tabs/tabs.ssr.test.tsx
+++ packages/@headlessui-react/src/components/tabs/tabs.ssr.test.tsx
@@ -7,9 +7,13 @@ beforeAll(() => {
   jest.spyOn(window, 'cancelAnimationFrame').mockImplementation(clearImmediate as any)
 })
 
-function Example({ defaultIndex = 0 }) {
+let spy: jest.SpyInstance<void, Parameters<typeof console.error>>
+beforeEach(() => (spy = jest.spyOn(console, 'error').mockImplementation(() => {})))
+afterEach(() => spy.mockRestore())
+
+function Example(props: { defaultIndex?: number; selectedIndex?: number }) {
   return (
-    <Tab.Group defaultIndex={defaultIndex}>
+    <Tab.Group {...props}>
       <Tab.List>
         <Tab>Tab 1</Tab>
         <Tab>Tab 2</Tab>
@@ -42,25 +46,70 @@ describe('Rendering', () => {
       expect(contents).toContain(`Content 2`)
       expect(contents).not.toContain(`Content 3`)
     })
-  })
 
-  // The hydration tests don't work in React 18 due to some bug in Testing Library maybe?
-  // Skipping for now
-  xdescribe('Hydration', () => {
-    it('should be possible to server side render the first Tab and Panel', async () => {
-      const { contents } = await renderHydrate(<Example />)
+    it('should be possible to server side render the selectedIndex=0 Tab and Panel', async () => {
+      let { contents } = await renderSSR(<Example selectedIndex={0} />)
 
       expect(contents).toContain(`Content 1`)
       expect(contents).not.toContain(`Content 2`)
       expect(contents).not.toContain(`Content 3`)
     })
 
-    it('should be possible to server side render the defaultIndex Tab and Panel', async () => {
-      const { contents } = await renderHydrate(<Example defaultIndex={1} />)
+    it('should be possible to server side render the selectedIndex=1 Tab and Panel', async () => {
+      let { contents } = await renderSSR(<Example selectedIndex={1} />)
 
       expect(contents).not.toContain(`Content 1`)
       expect(contents).toContain(`Content 2`)
       expect(contents).not.toContain(`Content 3`)
+    })
+  })
+
+  // The hydration tests don't work in React 18 due to some bug in Testing Library maybe?
+  // Skipping for now
+  describe.each([{ strict: true }, { strict: false }])('Hydration: %p', (opts) => {
+    it('should be possible to server side render the first Tab and Panel by default', async () => {
+      const { contents } = await renderHydrate(<Example />, opts)
+
+      expect(contents).toContain(`Content 1`)
+      expect(contents).not.toContain(`Content 2`)
+      expect(contents).not.toContain(`Content 3`)
+      expect(spy).not.toHaveBeenCalled()
+    })
+
+    it('should be possible to server side render the first Tab and Panel', async () => {
+      const { contents } = await renderHydrate(<Example defaultIndex={0} />, opts)
+
+      expect(contents).toContain(`Content 1`)
+      expect(contents).not.toContain(`Content 2`)
+      expect(contents).not.toContain(`Content 3`)
+      expect(spy).not.toHaveBeenCalled()
+    })
+
+    it('should be possible to server side render the defaultIndex Tab and Panel', async () => {
+      const { contents } = await renderHydrate(<Example defaultIndex={1} />, opts)
+
+      expect(contents).not.toContain(`Content 1`)
+      expect(contents).toContain(`Content 2`)
+      expect(contents).not.toContain(`Content 3`)
+      expect(spy).not.toHaveBeenCalled()
+    })
+
+    it('should be possible to server side render the selectedIndex=0 Tab and Panel', async () => {
+      let { contents } = await renderHydrate(<Example selectedIndex={0} />, opts)
+
+      expect(contents).toContain(`Content 1`)
+      expect(contents).not.toContain(`Content 2`)
+      expect(contents).not.toContain(`Content 3`)
+      expect(spy).not.toHaveBeenCalled()
+    })
+
+    it('should be possible to server side render the selectedIndex=1 Tab and Panel', async () => {
+      let { contents } = await renderHydrate(<Example selectedIndex={1} />, opts)
+
+      expect(contents).not.toContain(`Content 1`)
+      expect(contents).toContain(`Content 2`)
+      expect(contents).not.toContain(`Content 3`)
+      expect(spy).not.toHaveBeenCalled()
     })
   })
 })
