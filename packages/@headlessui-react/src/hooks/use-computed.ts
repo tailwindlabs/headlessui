006ceb9914fddfa58fca50eba5f87300661ceada
Marcus Mathiassen <mathiassenmarcus@me.com>
2023-02-10 18:46:42 +0100

    react: hooks: use-computed: remove dead dependencies

--- packages/@headlessui-react/src/hooks/use-computed.ts
+++ packages/@headlessui-react/src/hooks/use-computed.ts
@@ -5,6 +5,6 @@ import { useLatestValue } from './use-latest-value'
 export function useComputed<T>(cb: () => T, dependencies: React.DependencyList) {
   let [value, setValue] = useState(cb)
   let cbRef = useLatestValue(cb)
-  useIsoMorphicEffect(() => setValue(cbRef.current), [cbRef, setValue, ...dependencies])
+  useIsoMorphicEffect(() => setValue(cbRef.current), dependencies ?? [])
   return value
 }
