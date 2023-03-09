c0bec1fab1b4f783e0e13dac250c141111c7a886
Robin Malfait <malfait.robin@gmail.com>
2023-03-09 13:04:30 +0100

    do not set the `tabindex`
    The focus was always set, but the ring wasn't showing up. This was also
    focusing a ring when the browser decided not the add one.
    
    Let's make the browser decide when to show this or not.

--- packages/@headlessui-react/src/utils/focus-management.ts
+++ packages/@headlessui-react/src/utils/focus-management.ts
@@ -273,14 +273,5 @@ export function focusIn(
     next.select()
   }
 
-  // This is a little weird, but let me try and explain: There are a few scenario's
-  // in chrome for example where a focused `<a>` tag does not get the default focus
-  // styles and sometimes they do. This highly depends on whether you started by
-  // clicking or by using your keyboard. When you programmatically add focus `anchor.focus()`
-  // then the active element (document.activeElement) is this anchor, which is expected.
-  // However in that case the default focus styles are not applied *unless* you
-  // also add this tabindex.
-  if (!next.hasAttribute('tabindex')) next.setAttribute('tabindex', '0')
-
   return FocusResult.Success
 }
