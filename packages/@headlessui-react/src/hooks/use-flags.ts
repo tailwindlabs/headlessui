fc9a625414bd2dad084936e33926f7e86f5936ac
Robin Malfait <malfait.robin@gmail.com>
2023-03-16 16:30:58 +0100

    Fix "Can't perform a React state update on an unmounted component." when using the `Transition` component (#2374)
    * only change flags when mounted
    
    * update changelog

--- packages/@headlessui-react/src/hooks/use-flags.ts
+++ packages/@headlessui-react/src/hooks/use-flags.ts
@@ -1,12 +1,32 @@
 import { useState, useCallback } from 'react'
+import { useIsMounted } from './use-is-mounted'
 
 export function useFlags(initialFlags = 0) {
   let [flags, setFlags] = useState(initialFlags)
+  let mounted = useIsMounted()
 
-  let addFlag = useCallback((flag: number) => setFlags((flags) => flags | flag), [flags])
+  let addFlag = useCallback(
+    (flag: number) => {
+      if (!mounted.current) return
+      setFlags((flags) => flags | flag)
+    },
+    [flags, mounted]
+  )
   let hasFlag = useCallback((flag: number) => Boolean(flags & flag), [flags])
-  let removeFlag = useCallback((flag: number) => setFlags((flags) => flags & ~flag), [setFlags])
-  let toggleFlag = useCallback((flag: number) => setFlags((flags) => flags ^ flag), [setFlags])
+  let removeFlag = useCallback(
+    (flag: number) => {
+      if (!mounted.current) return
+      setFlags((flags) => flags & ~flag)
+    },
+    [setFlags, mounted]
+  )
+  let toggleFlag = useCallback(
+    (flag: number) => {
+      if (!mounted.current) return
+      setFlags((flags) => flags ^ flag)
+    },
+    [setFlags]
+  )
 
   return { flags, addFlag, hasFlag, removeFlag, toggleFlag }
 }
