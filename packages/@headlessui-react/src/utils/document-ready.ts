9b2349e489b8c33268b9e64677f7c4479e729bcd
Jordan Pittman <jordan@cryptica.me>
2023-03-21 19:25:14 -0400

    Refactor

--- packages/@headlessui-react/src/utils/document-ready.ts
+++ packages/@headlessui-react/src/utils/document-ready.ts
@@ -1,25 +1,12 @@
-function defer<T = unknown>() {
-  let resolve!: (value: T | PromiseLike<T>) => void
-  let reject!: (reason?: unknown) => void
-  const promise = new Promise<T>((_resolve, _reject) => {
-    resolve = _resolve
-    reject = _reject
-  })
-  return { promise, resolve, reject }
-}
-
-export function onDocumentReady() {
-  const ready = defer<void>()
-  const check = () => {
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-      ready.resolve()
-      document.removeEventListener('readystatechange', check)
-    }
+export function onDocumentReady(cb?: () => void) {
+  let check = () => {
+    if (document.readyState === 'loading') return
+    cb?.()
+    document.removeEventListener('DOMContentLoaded', check)
   }
 
-  document.addEventListener('readystatechange', check)
-
-  check()
-
-  return ready.promise
+  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+    document.addEventListener('DOMContentLoaded', check)
+    check()
+  }
 }
--- packages/@headlessui-vue/src/components/focus-trap/focus-trap.ts
+++ packages/@headlessui-vue/src/components/focus-trap/focus-trap.ts
@@ -210,28 +210,26 @@ export let FocusTrap = Object.assign(
 )
 
 let history: HTMLElement[] = []
-onDocumentReady().then(() => {
-  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-    function handle(e: Event) {
-      if (!(e.target instanceof HTMLElement)) return
-      if (e.target === document.body) return
-      if (history[0] === e.target) return
+onDocumentReady(() => {
+  function handle(e: Event) {
+    if (!(e.target instanceof HTMLElement)) return
+    if (e.target === document.body) return
+    if (history[0] === e.target) return
 
-      history.unshift(e.target)
+    history.unshift(e.target)
 
-      // Filter out DOM Nodes that don't exist anymore
-      history = history.filter((x) => x != null && x.isConnected)
-      history.splice(10) // Only keep the 10 most recent items
-    }
-
-    window.addEventListener('click', handle, { capture: true })
-    window.addEventListener('mousedown', handle, { capture: true })
-    window.addEventListener('focus', handle, { capture: true })
-
-    document.body.addEventListener('click', handle, { capture: true })
-    document.body.addEventListener('mousedown', handle, { capture: true })
-    document.body.addEventListener('focus', handle, { capture: true })
+    // Filter out DOM Nodes that don't exist anymore
+    history = history.filter((x) => x != null && x.isConnected)
+    history.splice(10) // Only keep the 10 most recent items
   }
+
+  window.addEventListener('click', handle, { capture: true })
+  window.addEventListener('mousedown', handle, { capture: true })
+  window.addEventListener('focus', handle, { capture: true })
+
+  document.body.addEventListener('click', handle, { capture: true })
+  document.body.addEventListener('mousedown', handle, { capture: true })
+  document.body.addEventListener('focus', handle, { capture: true })
 })
 
 function useRestoreElement(enabled: Ref<boolean>) {
