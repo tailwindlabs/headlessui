74c987351f5e1a17e46a0d682116ae83b719ce05
Robin Malfait <malfait.robin@gmail.com>
2023-04-26 14:56:06 +0200

    ensure `cb` in `useOnUnmount` is a stable reference

--- packages/@headlessui-react/src/hooks/use-on-unmount.ts
+++ packages/@headlessui-react/src/hooks/use-on-unmount.ts
@@ -1,7 +1,10 @@
 import { useRef, useEffect } from 'react'
 import { microTask } from '../utils/micro-task'
+import { useEvent } from './use-event'
 
 export function useOnUnmount(cb: () => void) {
+  let stableCb = useEvent(cb)
+
   let trulyUnmounted = useRef(false)
   useEffect(() => {
     trulyUnmounted.current = false
@@ -11,8 +14,8 @@ export function useOnUnmount(cb: () => void) {
       microTask(() => {
         if (!trulyUnmounted.current) return
 
-        cb()
+        stableCb()
       })
     }
-  }, [])
+  }, [stableCb])
 }
