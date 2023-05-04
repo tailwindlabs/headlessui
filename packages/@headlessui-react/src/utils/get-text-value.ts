652e6282d77eb5ca00ff88fbd7534d902d1a48d1
Robin Malfait <malfait.robin@gmail.com>
2023-05-04 13:23:55 +0200

    drop child nodes instead of replacing its innerText
    This makes it a bit slower but also more correct. We can use a cache on
    another level to ensure that we are not creating useless work.

--- packages/@headlessui-react/src/utils/get-text-value.ts
+++ packages/@headlessui-react/src/utils/get-text-value.ts
@@ -2,22 +2,13 @@ let emojiRegex =
   /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g
 
 function getTextContents(element: HTMLElement): string {
-  // Using innerText instad of textContent because:
+  // Using innerText instead of textContent because:
   //
   // > textContent gets the content of all elements, including <script> and <style> elements. In
   // > contrast, innerText only shows "human-readable" elements.
   // >
   // > — https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-  let value = element.innerText ?? ''
-
-  // Check if it contains some emojis or not, if so, we need to remove them
-  // because ideally we work with simple text values.
-
-  // Ideally we can use the much simpler RegEx: /\p{Extended_Pictographic}/u
-  // but we can't rely on this yet, so we use the more complex one.
-  if (emojiRegex.test(value)) {
-    value = value.replace(emojiRegex, '')
-  }
+  let currentInnerText = element.innerText ?? ''
 
   // Remove all the elements that shouldn't be there.
   //
@@ -25,15 +16,30 @@ function getTextContents(element: HTMLElement): string {
   // [aria-hidden]  — The screen reader doesn't see it
   // [role="img"]   — Even if it is text, it is used as an image
   //
-  // This is probably the slowest part, but if you want complete control over the text value,
-  // you better set an `aria-label` instead.
-  let children = element.querySelectorAll('[hidden],[aria-hidden],[role="img"]')
-  if (children.length > 0) {
-    for (let el of children) {
-      if (el instanceof HTMLElement) {
-        value = value.replace(el.innerText ?? '', '')
-      }
-    }
+  // This is probably the slowest part, but if you want complete control over the text value, then
+  // it is better to set an `aria-label` instead.
+  let copy = element.cloneNode(true)
+  if (!(copy instanceof HTMLElement)) {
+    return currentInnerText
+  }
+
+  let dropped = false
+  // Drop the elements that shouldn't be there.
+  for (let child of copy.querySelectorAll('[hidden],[aria-hidden],[role="img"]')) {
+    child.remove()
+    dropped = true
+  }
+
+  // Now that the elements are removed, we can get the innerText such that we can strip the emojis.
+  let value = dropped ? copy.innerText ?? '' : currentInnerText
+
+  // Check if it contains some emojis or not, if so, we need to remove them
+  // because ideally we work with simple text values.
+  //
+  // Ideally we can use the much simpler RegEx: /\p{Extended_Pictographic}/u
+  // but we can't rely on this yet, so we use the more complex one.
+  if (emojiRegex.test(value)) {
+    value = value.replace(emojiRegex, '')
   }
 
   return value
