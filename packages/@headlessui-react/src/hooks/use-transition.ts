c7fc02c9b09895a2e93c52ad12d4b0f85c80d57e
Robin Malfait <malfait.robin@gmail.com>
2023-08-04 11:27:04 +0200

    trigger immediate transition once the DOM is ready

--- packages/@headlessui-react/src/hooks/use-transition.ts
+++ packages/@headlessui-react/src/hooks/use-transition.ts
@@ -43,7 +43,29 @@ export function useTransition({
   useIsoMorphicEffect(() => {
     if (!immediate) return
 
-    latestDirection.current = 'enter'
+    d.nextFrame(() => {
+      latestDirection.current = 'enter'
+
+      let dd = disposables()
+      d.add(dd.dispose)
+
+      let node = container.current
+      if (!node) return // We don't have a DOM node (yet)
+      if (!mounted.current) return
+
+      dd.dispose()
+
+      onStart.current(latestDirection.current)
+
+      dd.add(
+        transition(node, classes.current, latestDirection.current === 'enter', () => {
+          dd.dispose()
+          onStop.current(latestDirection.current)
+        })
+      )
+
+      return dd.dispose
+    })
   }, [immediate])
 
   useIsoMorphicEffect(() => {
