44684795e6f6dfbdefd5a94ec716e9c065c749c2
Jordan Pittman <jordan@cryptica.me>
2023-08-29 12:32:32 -0400

    Specialize DOM helper to HTML elements
    Technically it could be an SVG element but much of Headless UI assumes HTML elements all over. So we’ll adjust the types to assume HTMLElement instead.

--- packages/@headlessui-vue/src/utils/dom.ts
+++ packages/@headlessui-vue/src/utils/dom.ts
@@ -1,10 +1,10 @@
 import { Ref, ComponentPublicInstance } from 'vue'
 
-type AsElement<T extends Element | ComponentPublicInstance> =
-  | (T extends Element ? T : Element)
+type AsElement<T extends HTMLElement | ComponentPublicInstance> =
+  | (T extends HTMLElement ? T : HTMLElement)
   | null
 
-export function dom<T extends Element | ComponentPublicInstance>(
+export function dom<T extends HTMLElement | ComponentPublicInstance>(
   ref?: Ref<T | null>
 ): AsElement<T> | null {
   if (ref == null) return null
