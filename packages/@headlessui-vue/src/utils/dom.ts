c0f36b29844a9f6c8d94927d7a45ef0d9c56f6ff
Jordan Pittman <jordan@cryptica.me>
2023-08-29 12:31:10 -0400

    Tweak `dom()` helper
    The helper can currently return a component instance when it should only ever return a DOM element. So, we fix the implementation to return null if it’s not an `Element` _and_ adjust the types such that if a `ComponentPublicInstance` is passed we change the return type to `Element`.

--- packages/@headlessui-vue/src/utils/dom.ts
+++ packages/@headlessui-vue/src/utils/dom.ts
@@ -1,8 +1,20 @@
 import { Ref, ComponentPublicInstance } from 'vue'
 
-export function dom<T extends Element | ComponentPublicInstance>(ref?: Ref<T | null>): T | null {
+type AsElement<T extends Element | ComponentPublicInstance> =
+  | (T extends Element ? T : Element)
+  | null
+
+export function dom<T extends Element | ComponentPublicInstance>(
+  ref?: Ref<T | null>
+): AsElement<T> | null {
   if (ref == null) return null
   if (ref.value == null) return null
 
-  return (ref.value as { $el?: T }).$el ?? ref.value
+  let el = (ref.value as { $el?: T }).$el ?? ref.value
+
+  if (el instanceof Element) {
+    return el as AsElement<T>
+  }
+
+  return null
 }
