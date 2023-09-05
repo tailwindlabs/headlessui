936d576b31d870bea7b96d12a7c43b1045b71659
Robin Malfait <malfait.robin@gmail.com>
2023-09-05 18:40:42 +0200

    use simpler `virtual` prop

--- packages/playground-react/pages/combobox/combobox-virtualized.tsx
+++ packages/playground-react/pages/combobox/combobox-virtualized.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Combobox } from '@headlessui/react'
+import React, { useState, useEffect, CSSProperties } from 'react'
+import { Combobox, Virtualizer } from '@headlessui/react'
 
 import { classNames } from '../../utils/class-names'
 import { Button } from '../../components/button'
@@ -38,6 +38,7 @@ export default function Home() {
         <div className="py-8 font-mono text-xs">Selected timezone: {activeTimezone}</div>
         <div className="space-y-1">
           <Combobox
+            virtual
             value={activeTimezone}
             onChange={(value) => {
               setActiveTimezone(value)
@@ -76,47 +77,49 @@ export default function Home() {
 
               <div className="absolute mt-1 w-full rounded-md bg-white shadow-lg">
                 <Combobox.Options className="shadow-xs max-h-60 overflow-auto rounded-md py-1 text-base leading-6 focus:outline-none sm:text-sm sm:leading-5">
-                  {timezones.map((timezone) => (
-                    <Combobox.Option
-                      key={timezone}
-                      value={timezone}
-                      className={({ active }) => {
-                        return classNames(
-                          'relative cursor-default select-none py-2 pl-3 pr-9 focus:outline-none',
-                          active ? 'bg-indigo-600 text-white' : 'text-gray-900'
-                        )
-                      }}
-                    >
-                      {({ active, selected }) => (
-                        <>
-                          <span
-                            className={classNames(
-                              'block truncate',
-                              selected ? 'font-semibold' : 'font-normal'
-                            )}
-                          >
-                            {timezone}
-                          </span>
-                          {selected && (
+                  {timezones.map((timezone) => {
+                    return (
+                      <Combobox.Option
+                        key={timezone}
+                        value={timezone}
+                        className={({ active }) => {
+                          return classNames(
+                            'w-full cursor-default select-none py-2 pl-3 pr-9 focus:outline-none',
+                            active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                          )
+                        }}
+                      >
+                        {({ active, selected }) => (
+                          <>
                             <span
                               className={classNames(
-                                'absolute inset-y-0 right-0 flex items-center pr-4',
-                                active ? 'text-white' : 'text-indigo-600'
+                                'block truncate',
+                                selected ? 'font-semibold' : 'font-normal'
                               )}
                             >
-                              <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path
-                                  fillRule="evenodd"
-                                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                  clipRule="evenodd"
-                                />
-                              </svg>
+                              {timezone}
                             </span>
-                          )}
-                        </>
-                      )}
-                    </Combobox.Option>
-                  ))}
+                            {selected && (
+                              <span
+                                className={classNames(
+                                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                                  active ? 'text-white' : 'text-indigo-600'
+                                )}
+                              >
+                                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </span>
+                            )}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    )
+                  })}
                 </Combobox.Options>
               </div>
             </div>
