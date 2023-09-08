572611e60f8255e85066c794ae067f4aa8143e43
Robin Malfait <malfait.robin@gmail.com>
2023-09-08 15:46:18 +0200

    tmp: disable production

--- packages/playground-vue/vite.config.js
+++ packages/playground-vue/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      isProduction: false,
+    }),
+  ],
 })
