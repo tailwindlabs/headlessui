e7377fafda3ac7f696aecc6b80ee9f14942f5d8f
Robin Malfait <malfait.robin@gmail.com>
2023-09-11 12:54:14 +0200

    add Combobox with virtual prop example

---
+++ packages/playground-vue/src/components/combobox/combobox-virtualized.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex">
+    <Example :virtual="true" />
+    <Example :virtual="false" />
+  </div>
+</template>
+
+<script setup>
+import Example from './_virtual-example.vue'
+</script>
