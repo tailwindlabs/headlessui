6aa57894807b574e3b2627e8f0f4f55345685d29
Robin Malfait <malfait.robin@gmail.com>
2024-04-04 01:54:03 +0200

    cleanup temporary test changes

--- packages/@headlessui-vue/src/components/transitions/transition.test.ts
+++ packages/@headlessui-vue/src/components/transitions/transition.test.ts
@@ -864,12 +864,6 @@ describe('Events', () => {
       let leaveDuration = 75
 
       withStyles(`
-        .enter-1, .enter-2, .leave-1, .leave-2 {
-          transition-property: opacity;
-          transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-          transition-duration: 150ms;
-        }
-
         .enter-1 { transition-duration: ${enterDuration * 1}ms; }
         .enter-2 { transition-duration: ${enterDuration * 2}ms; }
         .enter-from { opacity: 0%; }
