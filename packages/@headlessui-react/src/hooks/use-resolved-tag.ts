532f9124277f386b19c71e2f27307b33181cca48
Robin Malfait <malfait.robin@gmail.com>
2024-05-24 16:46:42 +0200

    add `useResolvedTag` hook
    This allows us to compute the `tag` name of a component. We can use a
    shortcut based on the `props.as` and/or the `DEFAULT_XXX_TAG` of a
    component. If this is not known/passed, then we compute it based on the
    `ref` instead which requires an actual re-render.

---
+++ packages/@headlessui-react/src/hooks/use-resolved-tag.ts
@@ -0,0 +1,33 @@
+import { useCallback, useState } from 'react'
+
+/**
+ * Resolve the actual rendered tag of a DOM node. If the `tag` provided is
+ * already a string we can use that as-is. This will happen when the `as` prop is
+ * not used or when it's used with a string value.
+ *
+ * If an actual component is used, then we need to do some more work because
+ * then we actually need to render the component to know what the tag name is.
+ */
+export function useResolvedTag<T extends React.ElementType>(tag: T) {
+  let tagName = typeof tag === 'string' ? tag : undefined
+  let [resolvedTag, setResolvedTag] = useState<string | undefined>(tagName)
+
+  return [
+    // The resolved tag name
+    tagName ?? resolvedTag,
+
+    // This callback should be passed to the `ref` of a component
+    useCallback(
+      (ref: any) => {
+        // Tag name is already known and it's a string, no need to re-render
+        if (tagName) return
+
+        if (ref instanceof HTMLElement) {
+          // Tag name is not known yet, render the component to find out
+          setResolvedTag(ref.tagName.toLowerCase())
+        }
+      },
+      [tagName]
+    ),
+  ] as const
+}
