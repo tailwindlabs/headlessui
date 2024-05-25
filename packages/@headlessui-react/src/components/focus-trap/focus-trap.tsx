77dc39f3c329dc38034bbba334be03367530be7b
Robin Malfait <malfait.robin@gmail.com>
2024-05-26 00:23:52 +0200

    start `FocusTrapFeatures.None` with `0` instead of `1`

--- packages/@headlessui-react/src/components/focus-trap/focus-trap.tsx
+++ packages/@headlessui-react/src/components/focus-trap/focus-trap.tsx
@@ -49,22 +49,22 @@ let DEFAULT_FOCUS_TRAP_TAG = 'div' as const
 
 export enum FocusTrapFeatures {
   /** No features enabled for the focus trap. */
-  None = 1 << 0,
+  None = 0,
 
   /** Ensure that we move focus initially into the container. */
-  InitialFocus = 1 << 1,
+  InitialFocus = 1 << 0,
 
   /** Ensure that pressing `Tab` and `Shift+Tab` is trapped within the container. */
-  TabLock = 1 << 2,
+  TabLock = 1 << 1,
 
   /** Ensure that programmatically moving focus outside of the container is disallowed. */
-  FocusLock = 1 << 3,
+  FocusLock = 1 << 2,
 
   /** Ensure that we restore the focus when unmounting the focus trap. */
-  RestoreFocus = 1 << 4,
+  RestoreFocus = 1 << 3,
 
   /** Initial focus should look for the `data-autofocus` */
-  AutoFocus = 1 << 5,
+  AutoFocus = 1 << 4,
 
   /** Enable all features. */
   All = InitialFocus | TabLock | FocusLock | RestoreFocus,
