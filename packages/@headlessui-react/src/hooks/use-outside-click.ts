f70dc65d4c9380c0b2d1fbc1cf3d2b6a3c646d44
Robin Malfait <malfait.robin@gmail.com>
2024-06-03 11:47:35 +0200

    make `handleOutsideClick` stable

--- packages/@headlessui-react/src/hooks/use-outside-click.ts
+++ packages/@headlessui-react/src/hooks/use-outside-click.ts
@@ -1,8 +1,9 @@
-import { useEffect, useRef, type MutableRefObject } from 'react'
+import { useCallback, useRef, type MutableRefObject } from 'react'
 import { FocusableMode, isFocusableElement } from '../utils/focus-management'
 import { isMobile } from '../utils/platform'
 import { useDocumentEvent } from './use-document-event'
 import { useIsTopLayer } from './use-is-top-layer'
+import { useLatestValue } from './use-latest-value'
 import { useWindowEvent } from './use-window-event'
 
 type Container = MutableRefObject<HTMLElement | null> | HTMLElement | null
@@ -15,101 +16,94 @@ export function useOutsideClick(
   cb: (event: MouseEvent | PointerEvent | FocusEvent | TouchEvent, target: HTMLElement) => void
 ) {
   let isTopLayer = useIsTopLayer(enabled, 'outside-click')
+  let cbRef = useLatestValue(cb)
 
-  // TODO: remove this once the React bug has been fixed: https://github.com/facebook/react/issues/24657
-  let enabledRef = useRef(false)
-  useEffect(
-    process.env.NODE_ENV === 'test'
-      ? () => {
-          enabledRef.current = isTopLayer
-        }
-      : () => {
-          requestAnimationFrame(() => {
-            enabledRef.current = isTopLayer
-          })
-        },
-    [isTopLayer]
-  )
-
-  function handleOutsideClick<E extends MouseEvent | PointerEvent | FocusEvent | TouchEvent>(
-    event: E,
-    resolveTarget: (event: E) => HTMLElement | null
-  ) {
-    if (!enabledRef.current) return
-
-    // Check whether the event got prevented already. This can happen if you use the
-    // useOutsideClick hook in both a Dialog and a Menu and the inner Menu "cancels" the default
-    // behavior so that only the Menu closes and not the Dialog (yet)
-    if (event.defaultPrevented) return
-
-    let target = resolveTarget(event)
-
-    if (target === null) {
-      return
-    }
-
-    // Ignore if the target doesn't exist in the DOM anymore
-    if (!target.getRootNode().contains(target)) return
-
-    // Ignore if the target was removed from the DOM by the time the handler was called
-    if (!target.isConnected) return
-
-    let _containers = (function resolve(containers): ContainerCollection {
-      if (typeof containers === 'function') {
-        return resolve(containers())
-      }
-
-      if (Array.isArray(containers)) {
-        return containers
-      }
-
-      if (containers instanceof Set) {
-        return containers
-      }
-
-      return [containers]
-    })(containers)
-
-    // Ignore if the target exists in one of the containers
-    for (let container of _containers) {
-      if (container === null) continue
-      let domNode = container instanceof HTMLElement ? container : container.current
-      if (domNode?.contains(target)) {
-        return
-      }
-
-      // If the click crossed a shadow boundary, we need to check if the container
-      // is inside the tree by using `composedPath` to "pierce" the shadow boundary
-      if (event.composed && event.composedPath().includes(domNode as EventTarget)) {
-        return
-      }
-    }
-
-    // This allows us to check whether the event was defaultPrevented when you are nesting this
-    // inside a `<Dialog />` for example.
-    if (
-      // This check allows us to know whether or not we clicked on a "focusable" element like a
-      // button or an input. This is a backwards compatibility check so that you can open a <Menu
-      // /> and click on another <Menu /> which should close Menu A and open Menu B. We might
-      // revisit that so that you will require 2 clicks instead.
-      !isFocusableElement(target, FocusableMode.Loose) &&
-      // This could be improved, but the `Combobox.Button` adds tabIndex={-1} to make it
-      // unfocusable via the keyboard so that tabbing to the next item from the input doesn't
-      // first go to the button.
-      target.tabIndex !== -1
+  let handleOutsideClick = useCallback(
+    function handleOutsideClick<E extends MouseEvent | PointerEvent | FocusEvent | TouchEvent>(
+      event: E,
+      resolveTarget: (event: E) => HTMLElement | null
     ) {
-      event.preventDefault()
-    }
+      if (!isTopLayer) return
 
-    return cb(event, target)
-  }
+      // Check whether the event got prevented already. This can happen if you
+      // use the useOutsideClick hook in both a Dialog and a Menu and the inner
+      // Menu "cancels" the default behavior so that only the Menu closes and
+      // not the Dialog (yet)
+      if (event.defaultPrevented) return
+
+      let target = resolveTarget(event)
+
+      if (target === null) {
+        return
+      }
+
+      // Ignore if the target doesn't exist in the DOM anymore
+      if (!target.getRootNode().contains(target)) return
+
+      // Ignore if the target was removed from the DOM by the time the handler
+      // was called
+      if (!target.isConnected) return
+
+      let _containers = (function resolve(containers): ContainerCollection {
+        if (typeof containers === 'function') {
+          return resolve(containers())
+        }
+
+        if (Array.isArray(containers)) {
+          return containers
+        }
+
+        if (containers instanceof Set) {
+          return containers
+        }
+
+        return [containers]
+      })(containers)
+
+      // Ignore if the target exists in one of the containers
+      for (let container of _containers) {
+        if (container === null) continue
+        let domNode = container instanceof HTMLElement ? container : container.current
+        if (domNode?.contains(target)) {
+          return
+        }
+
+        // If the click crossed a shadow boundary, we need to check if the
+        // container is inside the tree by using `composedPath` to "pierce" the
+        // shadow boundary
+        if (event.composed && event.composedPath().includes(domNode as EventTarget)) {
+          return
+        }
+      }
+
+      // This allows us to check whether the event was defaultPrevented when you
+      // are nesting this inside a `<Dialog />` for example.
+      if (
+        // This check allows us to know whether or not we clicked on a
+        // "focusable" element like a button or an input. This is a backwards
+        // compatibility check so that you can open a <Menu /> and click on
+        // another <Menu /> which should close Menu A and open Menu B. We might
+        // revisit that so that you will require 2 clicks instead.
+        !isFocusableElement(target, FocusableMode.Loose) &&
+        // This could be improved, but the `Combobox.Button` adds tabIndex={-1}
+        // to make it unfocusable via the keyboard so that tabbing to the next
+        // item from the input doesn't first go to the button.
+        target.tabIndex !== -1
+      ) {
+        event.preventDefault()
+      }
+
+      return cbRef.current(event, target)
+    },
+    [isTopLayer, cbRef]
+  )
 
   let initialClickTarget = useRef<EventTarget | null>(null)
 
   useDocumentEvent(
     'pointerdown',
     (event) => {
-      if (enabledRef.current) {
+      if (isTopLayer) {
         initialClickTarget.current = event.composedPath?.()?.[0] || event.target
       }
     },
@@ -119,7 +113,7 @@ export function useOutsideClick(
   useDocumentEvent(
     'mousedown',
     (event) => {
-      if (enabledRef.current) {
+      if (isTopLayer) {
         initialClickTarget.current = event.composedPath?.()?.[0] || event.target
       }
     },
