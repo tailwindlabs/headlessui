477f259cd3a6d305cad1099d7cd2355bb9865022
Robin Malfait <malfait.robin@gmail.com>
2024-08-01 15:44:42 +0200

    simplify `useDidElementMove`, only accept `HTMLElement | null`
    This doesn't support the `MutableRefObject<HTMLElement | null>` anymore.

--- packages/@headlessui-react/src/hooks/use-did-element-move.ts
+++ packages/@headlessui-react/src/hooks/use-did-element-move.ts
@@ -1,28 +1,16 @@
-import { useRef, type MutableRefObject } from 'react'
+import { useRef } from 'react'
 import { useIsoMorphicEffect } from './use-iso-morphic-effect'
 
-export function useDidElementMove(
-  enabled: boolean,
-  ref: MutableRefObject<HTMLElement | null> | HTMLElement | null
-) {
+export function useDidElementMove(enabled: boolean, element: HTMLElement | null) {
   let elementPosition = useRef({ left: 0, top: 0 })
 
   useIsoMorphicEffect(() => {
-    let el = ref === null ? null : ref instanceof HTMLElement ? ref : ref.current
-    if (!el) return
+    if (!element) return
 
-    let DOMRect = el.getBoundingClientRect()
+    let DOMRect = element.getBoundingClientRect()
     if (DOMRect) elementPosition.current = DOMRect
-  }, [enabled])
+  }, [enabled, element])
 
-  let element =
-    typeof window === 'undefined'
-      ? null
-      : ref === null
-        ? null
-        : ref instanceof HTMLElement
-          ? ref
-          : ref.current
   if (element == null) return false
   if (!enabled) return false
   if (element === document.activeElement) return false
