0cc873dac07287eef22d9de7c96e0473a08bea73
Pascal Sthamer <10992664+P4sca1@users.noreply.github.com>
2024-09-05 20:28:09 +0200

    feat: use native useId from Vue.js 3.5

--- packages/@headlessui-vue/src/hooks/use-id.ts
+++ packages/@headlessui-vue/src/hooks/use-id.ts
@@ -1,23 +1,15 @@
-import { inject, InjectionKey, provide } from 'vue'
+import { inject, InjectionKey, provide, useId as vueUseId } from 'vue'
 
-let GENERATE_ID: InjectionKey<() => string> = Symbol('headlessui.useid')
-
-let globalId = 0
+const GENERATE_ID: InjectionKey<() => string> = Symbol('headlessui.useid')
 
 export function useId() {
-  let generateId = inject(GENERATE_ID, () => {
-    return `${++globalId}`
+  return inject(GENERATE_ID, () => {
+    return vueUseId()
   })
-
-  return generateId()
 }
 
 /**
- * This function allows users to provide a custom ID generator
- * as a workaround for the lack of stable SSR IDs in Vue 3.x.
- *
- * This Nuxt users use the Nuxt provided `useId` function
- * which is stable across SSR and client.
+ * This function allows users to provide a custom ID generator.
  */
 export function provideUseId(fn: () => string) {
   provide(GENERATE_ID, fn)
