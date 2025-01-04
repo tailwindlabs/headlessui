ad92e007da0014eb6c34ab25df847d25475ad206
John Kevin Cadungog <johnkevin.cadungog@gmail.com>
2025-01-04 11:33:52 +0800

    feat: use vue.js native useId

--- packages/@headlessui-vue/src/hooks/use-id.ts
+++ packages/@headlessui-vue/src/hooks/use-id.ts
@@ -1,8 +1,22 @@
-let id = 0
-function generateId() {
-  return ++id
-}
+import * as Vue from 'vue'
 
-export function useId() {
-  return generateId()
+let GENERATE_ID: Vue.InjectionKey<() => string> = Symbol('headlessui.useid')
+let globalId = 0
+
+export const useId =
+  // Prefer Vue's `useId` if it's available.
+  // @ts-expect-error - `useId` doesn't exist in Vue < 3.5.
+  Vue.useId ??
+  function useId() {
+    let generateId = Vue.inject(GENERATE_ID, () => {
+      return `${++globalId}`
+    })
+
+    return generateId()
+  }
+/**
+ * This function allows users to provide a custom ID generator.
+ */
+export function provideUseId(fn: () => string) {
+  Vue.provide(GENERATE_ID, fn)
 }
