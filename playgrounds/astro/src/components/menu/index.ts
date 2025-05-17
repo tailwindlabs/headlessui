66d2e593a9bd70428aee8d87dafa795074e6a908
delano <delano@cpan.org>
2025-05-17 16:24:03 -0700

    Add Menu components and update index page

--- playgrounds/astro/src/components/menu/index.ts
+++ playgrounds/astro/src/components/menu/index.ts
@@ -3,3 +3,6 @@ export { default as Menu } from './Menu.astro';
 export { default as MenuButton } from './MenuButton.astro';
 export { default as MenuItems } from './MenuItems.astro';
 export { default as MenuItem } from './MenuItem.astro';
+export { default as MenuSection } from './MenuSection.astro';
+export { default as MenuHeading } from './MenuHeading.astro';
+export { default as MenuSeparator } from './MenuSeparator.astro';
