8afe84ddcb90e728d3a6cccaaaacf3a8e57aa206
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-09-01 12:26:57 +0200

    fix(vue): handle outsideClick touchend event on SVG elements (#3777)
    Closes: https://github.com/tailwindlabs/headlessui/issues/3752
    Reuses code from: https://github.com/tailwindlabs/headlessui/pull/3704
    
    Right now `touchend` only checks if target is HTMLElement, but this
    check will fail for some other elements (like SVG elements).
    
    This change brings dom utils from #3704 to Vue to properly resolve
    target.
    
    Before:
    
    
    https://github.com/user-attachments/assets/668b79df-b4d7-4287-acac-1c4a8777d6dd
    
    After:
    
    
    https://github.com/user-attachments/assets/92e328a8-0994-40df-b4ca-0ca610b81d72
    
    ---------
    
    Co-authored-by: Robin Malfait <malfait.robin@gmail.com>

--- packages/@headlessui-vue/src/utils/dom.ts
+++ packages/@headlessui-vue/src/utils/dom.ts
@@ -21,3 +21,38 @@ export function dom<T extends HTMLElement | ComponentPublicInstance>(
 
   return null
 }
+
+// Source: https://github.com/tailwindlabs/headlessui/blob/2de2779a1e3a5a02c1684e611daf5a68e8002143/packages/%40headlessui-react/src/utils/dom.ts
+// Normally you can use `element instanceof HTMLElement`, but if you are in
+// different JS Context (e.g.: inside an iframe) then the `HTMLElement` will be
+// a different class and the check will fail.
+//
+// Instead, we will check for certain properties to determine if the element
+// is of a specific type.
+
+export function isNode(element: unknown): element is Node {
+  if (typeof element !== 'object') return false
+  if (element === null) return false
+  return 'nodeType' in element
+}
+
+export function isElement(element: unknown): element is Element {
+  return isNode(element) && 'tagName' in element
+}
+
+export function isHTMLElement(element: unknown): element is HTMLElement {
+  return isElement(element) && 'accessKey' in element
+}
+
+// HTMLOrSVGElement doesn't inherit from HTMLElement or from Element. But this
+// is the type that contains the `tabIndex` property.
+//
+// Once we know that this is an `HTMLOrSVGElement` we also know that it is an
+// `Element` (that contains more information)
+export function isHTMLorSVGElement(element: unknown): element is HTMLOrSVGElement & Element {
+  return isElement(element) && 'tabIndex' in element
+}
+
+export function isHTMLIframeElement(element: unknown): element is HTMLIFrameElement {
+  return isHTMLElement(element) && element.nodeName === 'IFRAME'
+}
