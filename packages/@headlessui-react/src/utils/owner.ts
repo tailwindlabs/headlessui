4ad6f68a81c3e895f3a59cc5fd8c43d2de16c62c
Robin Malfait <malfait.robin@gmail.com>
2025-09-16 17:14:07 +0200

    add new helpers
    Essentially when dealing with shadow root DOM nodes, using the
    `ownerDocument` is not enough. Because if you look at the
    `ownerDocument.activeElement` then it would point to the shadow DOm
    element itself instead of an element inside.
    
    This adds a few helpers to:
    
    1. `getRootNode` — which is going to point at the shadow DOM element
       instead of a parent ownerDocument.
    2. `getActiveElement` — given a DOM node, it can figure out the
       activeElement within its environment (not sure what to call this).
       But this way we can figure out the active element inside the shadow
       DOM element instead of the shadow DOM element itself.
    3. `isActiveElement` — uses the above, but simplifies the usage a bit:
    
        ```diff
        - getActiveElement(machine.inputRef.current) === machine.inputRef.current
        + isActiveElement(machine.inputRef.current)
        ```

--- packages/@headlessui-react/src/utils/owner.ts
+++ packages/@headlessui-react/src/utils/owner.ts
@@ -5,9 +5,28 @@ export function getOwnerDocument<T extends Element | MutableRefObject<Element | 
   element: T | null | undefined
 ): Document | null {
   if (env.isServer) return null
-  if (!element) return document
-  if ('ownerDocument' in element) return element.ownerDocument
-  if ('current' in element) return element.current?.ownerDocument ?? document
+  if (element == null) return document
 
-  return null
+  let target: Element | null = 'current' in element ? element.current : element
+  return target?.ownerDocument ?? document
+}
+
+export function getRootNode<T extends Element | MutableRefObject<Element | null>>(
+  element: T | null | undefined
+): Document | ShadowRoot | null {
+  if (env.isServer) return null
+  if (element == null) return document
+
+  let target: Element | null = 'current' in element ? element.current : element
+
+  // @ts-expect-error `getRootNode`'s return type is typed as `Node`, but we want to type it a bit better
+  return target?.getRootNode?.() ?? document
+}
+
+export function getActiveElement(element: Element | null | undefined): Element | null {
+  return getRootNode(element)?.activeElement ?? null
+}
+
+export function isActiveElement(element: Element | null | undefined): boolean {
+  return getActiveElement(element) === element
 }
